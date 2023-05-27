61211b4db8018a9864498ae9dd84231e476cf467
Eduardo Orellana <rtugdhkch@gmail.com>
2023-05-26 23:35:41 -0600

    Libreria

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using libreria;
+using libreria.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSqlServer<LibreriaContext>("Data Source=localhost; Initial Catalog=libreriaDB;user id=sa;password=Programaci0n$;Encrypt=False");
+builder.Services.AddScoped<IAutorService, AutorService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    //Documentacion del REST API
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+//app.MapGet(.....)
+//por
+app.MapControllers();
+
+app.Run();
